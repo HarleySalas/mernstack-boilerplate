3eb3ce7edf998c76504a6f3262eea52bc1f92576
Harley Salas <harley@harleysalas.com>
2019-09-14 23:27:29 +0300

    fixed some security issues

--- server/src/config/services/express.js
+++ server/src/config/services/express.js
@@ -4,7 +4,11 @@ const config = require('./config');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const cors = require('cors');
+const helmet = require('helmet');
 
 //Express Configuration
 const init = () => {
@@ -21,13 +25,18 @@ const init = () => {
   // app.use('/build/static', express.static(config.clientStaticFolder));
   // app.use('/build', express.static(config.clientBuildFolder));
   app.use(cookieParser());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+  app.use(bodyParser.urlencoded({ extended: true }));
   // app.use(bodyParser.json());
-  // app.use(express.json());
-  // app.use(express.urlencoded({ extended: true, limit: '10kb' }))
+  const limiter = rateLimit({
+    max: 250,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP. Please try again in an hour.'
+  })
+  app.use('/api', limiter);
+  app.use(mongoSanitize());
+  app.use(xss());
   app.use(cors(corsOptions));
+  app.use(helmet())
   return app;
 }
 
